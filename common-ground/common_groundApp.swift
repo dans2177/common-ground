ab0b6ab120c16f7ffe0f83524115452276c01fa6
dans2177 <info@shemon-industries.com>
2026-03-09 17:50:27 -0700

    Initial Commit

---
+++ common-ground/common_groundApp.swift
@@ -0,0 +1,17 @@
+//
+//  common_groundApp.swift
+//  common-ground
+//
+//  Created by Daniel Shemon on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct common_groundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
